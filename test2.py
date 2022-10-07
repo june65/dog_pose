f24e0fcae3772ea46f3b4d3eb1591ccf1256c4a5
june0605 <june0100726@gmail.com>
2022-10-07 15:32:14 +0900

    Add dog_pose_v4

--- test2.py
+++ test2.py
@@ -2,12 +2,17 @@ import glob
 import tensorflow as tf
 import pandas as pd
 from keras.applications.inception_resnet_v2 import preprocess_input
-
-model = tf.keras.models.load_model('./dog_pose_v3')
+import shutil
+model = tf.keras.models.load_model('./dog_pose_v4')
 #filepaths =  'data/val_data_set/not_pose/n02085620_575.jpg'
 
+sigor_pose_path = 'C:/Users/june65/Desktop/dog_pose/data/wrong/sigor_pose'
+not_pose_path = 'C:/Users/june65/Desktop/dog_pose/data/wrong/not_pose'
 
-dog_images= glob.glob('.\\data\\val_data_set\\sigor_pose\\*')
+dog_images= glob.glob('.\\data\\train_data_set\\not_pose\\*')
+
+score = [0,0]
+i = 0
 
 for dog_image in dog_images:
     print(dog_image)
@@ -20,8 +25,19 @@ for dog_image in dog_images:
     test_image = test_image.reshape((1, test_image.shape[0], test_image.shape[1], test_image.shape[2]))
     test_image = preprocess_input(test_image)
     prediction = model.predict(test_image)
-
+    
     df = pd.DataFrame({'pred':prediction[0]})
     df = df.sort_values(by='pred', ascending=False, na_position='first')
     print(f"## 예측률 : {(df.iloc[0]['pred'])* 100:.2f}%")
-    print([df[df == df.iloc[0]].index[0]])
+    print(df[df == df.iloc[0]].index[0])
+
+    if df[df == df.iloc[0]].index[0] <= 0.5 :
+        score[0] += 1    
+        #shutil.move(dog_image, sigor_pose_path)
+    else :
+        score[1] += 1
+        shutil.move(dog_image, not_pose_path)
+        
+    i += 1
+
+print(score) 
